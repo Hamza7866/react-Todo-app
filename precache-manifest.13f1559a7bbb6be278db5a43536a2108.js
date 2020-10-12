ce37cb607c0241bc56b4e59728d94438f2458490
Hamza Muhammad <hmuhammad@inths.org>
2020-10-11 23:36:12 -0400

    Updates

---
+++ precache-manifest.13f1559a7bbb6be278db5a43536a2108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bff6a55ab9ecd2adc562948ece51553",
+    "url": "/react-Todo-app/index.html"
+  },
+  {
+    "revision": "0af66125e5c27a192008",
+    "url": "/react-Todo-app/static/css/main.3f31fe86.chunk.css"
+  },
+  {
+    "revision": "e6c5f9755b0c348d7302",
+    "url": "/react-Todo-app/static/js/2.78de81ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-Todo-app/static/js/2.78de81ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0af66125e5c27a192008",
+    "url": "/react-Todo-app/static/js/main.987a0274.chunk.js"
+  },
+  {
+    "revision": "5bc777ea3de3ad304be3",
+    "url": "/react-Todo-app/static/js/runtime-main.d18e4ca8.js"
+  }
+]);
